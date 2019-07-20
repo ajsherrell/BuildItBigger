6ed289f52485d5d09c90ecea060658e234142099
ajsherrell <amiesherrell@gmail.com>
2019-07-20 12:27:15 -0400

    Add interstitial ad code

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAndroidTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAndroidTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class OpenJokeDisplayActivityTest {
+public class EndpointsAndroidTest {
 
     public static final String EMPTY_STRING = "";
 
--- app/src/free/java/com/udaciy/gradle/builditbigger/free/MainActivityFragment.java
+++ app/src/free/java/com/udaciy/gradle/builditbigger/free/MainActivityFragment.java
@@ -2,14 +2,35 @@ package com.udaciy.gradle.builditbigger.free;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 
 import com.udacity.gradle.builditbigger.R;
 
+
 public class MainActivityFragment extends AppCompatActivity {
 
+    public MainActivityFragment() {
+    }
+
+    private InterstitialAd mInterstitial;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jokes_display);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.activity_main_fragment, container, false);
+
+        MobileAds.initialize(getActivity().getApplicationContext(), getString(R.string.ad_app_id));
+
+        mInterstitial = new InterstitialAd(getActivity().getApplicationContext());
+        mInterstitial.setAdUnitId(R.string.interstitial_ad_unit_id);
+        mInterstitial.loadAd(new AdRequest.Builder().build());
+        // TODO: connect interstitialAd to root
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+        mAdView.loadAd(adRequest);
+        return root;
     }
 }
